6ed15685b4c1f204a25d71fc1379723505e2713a
Asif Mia <asifmia.dev26@gmail.com>
2026-01-14 01:14:58 +0600

    Add Destination Section in homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Hero from "../components/HomeComponents/Hero";
+import TopDestination from "../components/HomeComponents/TopDestination";
 
 const Homepage = () => {
   return (
     <Layout>
       <div>
         <Hero />
+        <TopDestination />
       </div>
     </Layout>
   );
